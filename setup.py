06c85e805ad6564f8c8cf06fadda4ef128068254
Gaffer Fitch <gaffer.fitch@gmail.com>
2018-11-25 22:46:01 -0500

    Try this?

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 
 from setuptools import setup, find_packages
 setup(
-    name="pi_k8s_fitches.chore_redis",
+    name="pi-k8s-fitches-chore-redis",
     version="0.1",
     packages=["pi_k8s_fitches"],
     package_dir={'pi_k8s_fitches':'lib'},
