0b7feded42edfb664fc631d0f6ba7ad248130a0e
Gaffer Fitch <gaffer.fitch@gmail.com>
2018-11-25 22:05:35 -0500

    Proper package

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 setup(
     name="pi_k8s_fitches.chore_redis",
     version="0.1",
-    packages=["pi_k8s_fitches"],
+    packages=["pi_k8s_fitches.chores_redis"],
     package_dir={'pi_k8s_fitches':'lib'},
     install_requires=[
         "redis==2.10.6"
